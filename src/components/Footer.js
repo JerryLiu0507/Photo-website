99e625d5d9874d8ca60a6233a7918b4447117524
Jerry Liu <jerryliu0507@gmail.com>
2021-10-12 20:13:36 +0800

    add few files

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+    return<div className="footer">Jerry Liu 2021©</div>;
+};
+
+export default Footer;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div style={{minHeight:"100vh"}}>
+            <h1>About</h1>
+            <p>This is for test.</p>
+        </div>
+    )
+}
+
+export default About;
